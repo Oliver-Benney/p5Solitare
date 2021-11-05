e0dd8a4559c3759e32767dcfc84052fb08303437
Oliver-Benney <oliver.benney@students.plymouth.ac.uk>
2021-11-05 16:28:58 +0000

    solitare

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,34 @@
+//global variables
+var cardX,cardY,cardHeight,cardWidth;
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(600, 400);
+    background(0,100,0);
     canvas.parent("p5container");
 }
 
 function draw() {
-    background(220);
+  background(r,g,b) 
+  
+    //vars
+  cardX= width/2;
+  cardY= height/2;
+  cardWidth=180;
+  cardHeight=180;
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    quad(mouseX-40,mouseY-60,mouseX+40,mouseY-60,mouseX+40,mouseY+60,mouseX-40,mouseY+60)
+
+    fill(255,0,0)
+    ellipse(mouseX,mouseY,20)
+
+    fill(255,0,0);
+    textSize(25);
+    text('A', mouseX-35, mouseY-36);
+
+    fill(255,0,0);
+    textSize(25);
+    text('A', mouseX+19, mouseY+55);
+
+  
 }
